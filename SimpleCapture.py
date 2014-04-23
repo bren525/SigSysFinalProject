4b8804ca0148829563fd4e89f7d490104a2e0edd
bren525 <bren525@gmail.com>
2014-04-23 12:01:44 -0700

    changed fft2test back

--- SimpleCapture.py
+++ SimpleCapture.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('colour.wmv')
 plt.ion()
 
 while(True):
@@ -12,16 +13,16 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     f = np.fft.fft2(gray)
-	fshift = np.fft.fftshift(f)
-	magnitude_spectrum = 20*np.log(np.abs(fshift))
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
 
 
-	plt.subplot(121),plt.imshow(magnitude_spectrum, cmap = 'gray')
-	plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-	plt.subplot(122),plt.imshow(gray, cmap = 'gray')
-	plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-	plt.draw()
-	'''
+    plt.subplot(121),plt.imshow(magnitude_spectrum, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(gray, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.draw()
+    '''
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(10) & 0xFF == ord('q'):
--- fft2test.py
+++ fft2test.py
@@ -14,16 +14,3 @@ plt.subplot(121),plt.imshow(img, cmap = 'gray')
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
 plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-
-plt.ion()
-for i in range(10):
-	plt.subplot(121),plt.imshow(magnitude_spectrum, cmap = 'gray')
-	plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-	plt.draw()
-	time.sleep(1)
-	plt.subplot(121),plt.imshow(img, cmap = 'gray')
-	plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-	plt.draw()
-	time.sleep(1)
-
-plt.close()
