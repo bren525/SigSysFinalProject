85ee018ef68bc33bdcac427018c80a87b1142b32
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2014-04-24 14:09:25 -0400

    Changed

--- SimpleCapture.py
+++ SimpleCapture.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 cap = cv2.VideoCapture(0)
 plt.ion()
@@ -12,20 +13,20 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     f = np.fft.fft2(gray)
-	fshift = np.fft.fftshift(f)
-	magnitude_spectrum = 20*np.log(np.abs(fshift))
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
 
 
-	plt.subplot(121),plt.imshow(magnitude_spectrum, cmap = 'gray')
-	plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-	plt.subplot(122),plt.imshow(gray, cmap = 'gray')
-	plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-	plt.draw()
-	'''
+    plt.subplot(121),plt.imshow(magnitude_spectrum, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(gray, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.draw()
+    '''
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+    break
     '''
 
 # When everything done, release the capture
