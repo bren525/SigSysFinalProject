7b07eb1895db16df64940c6ea896435ab9e8f503
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2014-05-01 18:45:06 -0400

    Work

--- WebCamToSound.py
+++ WebCamToSound.py
@@ -6,8 +6,20 @@ import wave
 import sys
 from scipy.io.wavfile import read,write
 
+def square(start, width):
+    list = []
+    for i in range(0,600):
+        if i >= start and i < start + width:
+            list.append(100)
+        else:
+            list.append(0)
+
+    return np.array(list)
+
+CHUNK = 1024
 cap = cv2.VideoCapture(0)
 plt.ion()
+j = 300
 while(1):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -15,24 +27,32 @@ while(1):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    freq = gray[len(gray)/2]
+    #freq = gray[len(gray)/2]
 
+    freq = square(j,10);
+    j = ((j+5) % 590)
+
+    plt.clf()
     plt.subplot(121),plt.plot(freq)
 
     time = np.fft.ifft(freq)
 
-    plt.subplot(122),plt.plot(time)
+    plt.subplot(122),plt.plot(time[50:550])
     plt.draw()
 
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
 
-    someSound = np.real(time)
+
+    someSound = np.real(time[50:550])
     print someSound
-    for i in range(0,len(gray)):
+    for i in range(0,100):
     	#freq = f[i]
     	#time = np.fft.ifft(freq)
     	someSound = np.concatenate((someSound,np.real(time)),0)
 
-    write("cam.wav",4410,someSound);
+    write("cam.wav",44100,someSound);
 
     wf = wave.open("cam.wav", 'rb')
 
