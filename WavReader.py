53240ecf118c878cf417e4154538c15ff08cb0b0
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2014-05-02 22:11:50 -0400

    Significant hacking code should be ready

--- WavReader.py
+++ WavReader.py
@@ -2,6 +2,27 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from scipy.io.wavfile import read,write
+import subprocess
+from matplotlib import pyplot as plt
 
 music = read("DumbKid.wav")
-print music
+
+
+sound = []
+for i in range(len(music[1])):
+    sound.append(music[1][i][0])
+
+freq = np.fft.fft(sound)
+
+plt.clf()
+plt.subplot(122),plt.plot(freq)
+
+
+plt.subplot(121),plt.plot(sound)
+plt.show()
+
+sound = np.array(sound)
+
+write("DumbCopy.wav",44100,sound)
+
+subprocess.call(["aplay","DumbCopy.wav"])
